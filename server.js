c16e3611307649be6c85b9bdf5fbb3772713fe9e
unknown <jan.weitzel@gmail.com>
2023-02-10 04:48:14 +0100

    second commit

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var corsOptions = {
 }
 
 app.get('/', cors(corsOptions), function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for only example.com.'})
+  res.json({msg: 'This is CORS-enabled for zahlendreher node backend.'})
 })
 
 app.listen(8080, function () {
